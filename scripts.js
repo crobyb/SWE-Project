a5ca5cf0171657883b3b7ab6c3287bccfe0ee0d7
Nick Bear <99135011+NicholasBear97@users.noreply.github.com>
2023-04-29 22:29:23 -0400

    Fix Button functionality
    Add logic to capture user text input and send to listener url

--- scripts.js
+++ scripts.js
@@ -1,19 +1,31 @@
 //configure data label, type, and value
-function data_director(element) {
-    var dataLabel = element.id;
-    var data = document.getElementById(dataLabel).value;
+function data_director(buttonId) {
+
+    if (buttonId == "zip_regular_search" || buttonId == "zip_regular_search") {
+        const arr = document.getElementsByTagName('input');
+        var data;
+        for (let i = 0; i < arr.length; i++) {
+            console.log(arr[i].value);
+            if (arr[i].value != "") {
+                data = arr[i].value;
+            }
+        }
+    } else {
+        data = null;
+    }
+    console.log(data);
     var data_container = { data };
     var converter = JSON.stringify(data_container);
-    data_bus(dataLabel, converter);
+    data_bus(converter);
 }
 
 //interaction between JavaScript and python file
-function data_bus(dataLabel, converter) {
+function data_bus(converter) {
 
     //if no data in converter, send data to user
     if (!converter) {
         //need server url? trying movie server
-        const myRequest = new Request("export.json");
+        const myRequest = new Request("movies.json");
         fetch(myRequest)
             .then((response) => response.json())
             .then((return_data) => {
