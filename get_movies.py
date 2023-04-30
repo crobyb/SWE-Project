f6e7e011c54972479657f6def6372771efe85a53
crobyb <124478490+crobyb@users.noreply.github.com>
2023-04-29 22:10:36 -0400

    Update get_movies.py

--- get_movies.py
+++ get_movies.py
@@ -1,5 +1,4 @@
 import mysql.connector
-import json
 
 def connect_to_db():
     try:
@@ -26,6 +25,46 @@ def query_database(conn, query):
     result = cursor.fetchall()
     return result
 
+# Example usage:
+conn = connect_to_db()
+if conn:
+    # Query the database
+    query = "SELECT * FROM movies"
+    result = query_database(conn, query)
+    if result:
+        print("Results:")
+        for row in result:
+            print(row)
+    else:
+        print("No results")
+    conn.close()
+=======
+import mysql.connector
+
+def connect_to_db():
+    try:
+        # Connect to MySQL database
+        conn = mysql.connector.connect(
+            host="db-mysql-nyc1-30956-do-user-3263304-0.b.db.ondigitalocean.com",  # replace with your MySQL host
+            user="doadmin",  # replace with your MySQL username
+            password="AVNS_GHgC3IFDsdQ2Uuivp0s",  # replace with your MySQL password
+            port="25060",
+            auth_plugin='mysql_native_password',
+            database="defaultdb"  # replace with your MySQL database name
+            # sslmode='REQUIRED'
+        )
+        if conn.is_connected():
+            print("Connected to MySQL database")
+            return conn
+    except mysql.connector.Error as e:
+        print(f"Error connecting to MySQL database: {e}")
+        return None
+
+def query_database(conn, query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return result
 
 # Example usage:
 conn = connect_to_db()
@@ -33,7 +72,10 @@ if conn:
     # Query the database
     query = "SELECT * FROM movies"
     result = query_database(conn, query)
-    json_object = json.dumps(result, indent=4, default=str)
-    with open("export.json", "w") as outfile:
-        outfile.write(json_object)
+    if result:
+        print("Results:")
+        for row in result:
+            print(row)
+    else:
+        print("No results")
     conn.close()
