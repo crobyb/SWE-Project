cc66bb9c985f2248c3d131996610195728876bea
tomatoromano <124543921+tomatoromano@users.noreply.github.com>
2023-04-29 22:36:38 -0400

    Update API.py

--- API.py
+++ API.py
@@ -1,18 +1,31 @@
 # CS_KSU
 import googlemaps
 from datetime import datetime
+from flask import Flask, render_template, request
 
+app = Flask(__name__, static_url_path = '/temp2\stat2', static_folder= "temp2\stat2")
+
+@app.route("/")
+def index():
+    return render_template("Project.html")
+
+@app.route("/Project.html", methods=["POST"])
+def search():
+    location = request.form["location"]
 
 gmaps = googlemaps.Client(key='AIzaSyDsXXZgg7G1hz39Bfb-j5WiUXwxlY_aPQo')
 
-# Get your current location
-current_location = gmaps.geolocate()
+# Get the user's location 
+#address = input("Enter an address:") 
+
+# Geocode the address to get its latitude and longitude
+geocode_result = gmaps.geocode(address)
+location = geocode_result[0]['geometry']['location']
 
 # Define the search parameters
 search_params = {
-    'location': f"{current_location['lat']},{current_location['lng']}",
-    # Search within 5 km
-    'radius': 5000, 
+    'location': f"{location['lat']},{location['lng']}",
+    'radius': 5000,
     'type': 'movie_theater'
 }
 
